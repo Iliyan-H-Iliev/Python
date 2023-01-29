c18039e8b1f144195f359255a0c7f2858113dcff
Iliyan-H-Iliev <iliyan.hr.iliev@gmail.com>
2023-01-29 21:59:42 +0000

    update

--- Fundamentals/Functions/absolute_values.py
+++ Fundamentals/Functions/absolute_values.py
@@ -1,6 +1,14 @@
-number_list = input().split()
+numbers_list = input().split()
 
 absolute_values = []
 
-def abs_values():
-    for el in
+
+def abs_values(str_list):
+    for el in str_list:
+        absolute_values.append(abs(float(el)))
+
+    return absolute_values
+
+
+abs_values(numbers_list)
+print(absolute_values)
