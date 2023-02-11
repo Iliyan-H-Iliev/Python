9d3dd08f31f2d048f275515ee3f98222fc395df5
Iliyan Hristov Iliev <iliyan.hr.iliev@gmail.com>
2023-02-11 13:51:56 +0000

    update

--- Fundamentals/List_Advanced/electron_distribution.py
+++ Fundamentals/List_Advanced/electron_distribution.py
@@ -1,6 +1,7 @@
 electrons = int(input())
 result = []
 position_atom_shel = 1
+
 while electrons > 0:
     electrons_in_atom_shel = 2 * (position_atom_shel**2)
     result.append(min(electrons_in_atom_shel, electrons))
