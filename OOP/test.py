fc67a487082dc8ef602072ecb3e6c3ac37c4568a
Iliyan-H-Iliev <iliyan.hr.iliev@gmail.com>
2023-09-24 12:58:28 +0100

    update

--- OOP/test.py
+++ OOP/test.py
@@ -1,48 +1,31 @@
-from typing import List
+def myXOR(x, y):
+    res = 0  # Initialize result
+
+    # Assuming 32-bit Integer
+    for i in range(31, -1, -1):
+
+        # Find current bits in x and y
+        b1 = x & (1 << i)
+        b2 = y & (1 << i)
+        b1 = min(b1, 1)
+        b2 = min(b2, 1)
+
+        # If both are 1 then 0
+        # else xor is same as OR
+
+        xoredBit = 0
+        if (b1 & b2):
+            xoredBit = 0
+        else:
+            xoredBit = (b1 | b2)
+
+        # Update result
+        res <<= 1;
+        res |= xoredBit
+    return res
 
 
-class Book:
-    def __init__(self, title, author):
-        self.title = title
-        self.author = author
-        self.page = 0
-
-    def turn_page(self, page):
-        self.page = page
-
-    def __str__(self):
-        return f"This is a book with title - ' {self.title} ' written by {self.author}."
-
-
-class Library:
-    def __init__(self, name):
-        self.name = name
-        self.books: List = [Book]
-
-    def add_book(self, book: Book):
-        if book not in self.books:
-            self.books.append(book)
-            return f"Book with title {book.title} and author {book.author} successfully added to {self.name} library"
-
-        return f"This book is already in the library!"
-
-    def find_book(self, title):
-        try:
-            curr_book = next(filter(lambda b: b.title == title, self.books))
-            return curr_book
-        except StopIteration:
-            raise ValueError("This book is not in the library")
-
-
-a = Book("Harry Potter and the Philosopher's Stone", "J. K. Rowling")
-library = Library("Downtown")
-print(library.add_book(a))
-print(library.add_book(a))
-print(library.find_book("Harry Potter and the Philosopher's Stone"))
-
-
-
-        players_info = '\n'.join([f"Player: {player.name}, {player.age}, {player.stamina}, {player.need_sustenance}" for player in self.players])
-        supplies_info = '\n'.join([f"{supply.__class__.__name__}: {supply.name}, {supply.energy}" for supply
-                                   in self.supplies])
-        return f"{players_info}\n{supplies_info}"
+# Driver Code
+x = 95
+y = 74
+print("XOR is", myXOR(x, y))
